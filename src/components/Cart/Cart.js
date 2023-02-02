9c799295c0e9d23785db90d8f6f41dd59c52dd3e
Svetlana Tomašik <seka.corazon@gmail.com>
2023-02-02 11:04:03 +0100

    shopping cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+// Cart components should render all the Cart items
+// and display the total amount. (in the modal)
+
+import classes from "./Cart.module.css";
+
+const Cart = () => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.action}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
