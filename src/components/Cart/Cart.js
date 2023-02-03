58933edc4b6c3bbe875126d65e3ad14d5d5b1318
Svetlana Tomašik <seka.corazon@gmail.com>
2023-02-03 14:01:02 +0100

    making items removable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -14,9 +14,13 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemRAddHandler = (item) => {};
+  const cartItemRAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
