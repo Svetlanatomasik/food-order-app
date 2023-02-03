6c91ad5a6a8125fbe6b7a27aa73f0b00e57482df
Svetlana Tomašik <seka.corazon@gmail.com>
2023-02-03 12:12:03 +0100

    working with refs and context

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,21 @@
+import { useContext } from "react";
+
 import classes from "./MealItem.module.css";
 import MealItemFrom from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -11,7 +24,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <MealItemFrom id={props.id} />
+      <MealItemFrom id={props.id} onAddToCart={addToCartHandler} />
     </li>
   );
 };
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,16 +1,17 @@
+import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
 
       {/* {...props.input} - This ensures that all the key value pairs
       in this input object,
       which we receive on props input are added as props to input. */}
     </div>
   );
-};
+});
 
 export default Input;
